bd5c8e5ca5bbde97909a0f36df1c5dbcb1cf53e1
Juan <juandgxiii@users.noreply.github.com>
2018-08-07 20:55:26 -0500

    csv y html de resultados

--- sfc line.js
+++ sfc line.js
@@ -20,7 +20,7 @@ function draw(datos_global) {
   const num = datos['fechas_js'].length;
 
   const escalaY = d3.scaleLinear()
-    .domain([d3.min(datos['cartera_total']) * 0.9, d3.max(datos['cartera_total'])])
+    .domain([d3.min(datos['resultados']) * 0.9, Math.max(0, d3.max(datos['resultados']))])
     .range([h - paddingY, 0]);
   const escalaX = d3.scaleTime()
     .domain([datos['fechas_js'][0], datos['fechas_js'][num - 1].setMonth(datos['fechas_js'][num - 1].getMonth() + 1)])
@@ -49,63 +49,6 @@ function draw(datos_global) {
   .attr('class', 'grid')
   .call(gridlines);
 
-  let bars = svg_cart
-    .selectAll('rect')
-    .data(datos['cartera_total'])
-    .enter()
-    .append('rect')
-
-    bars
-    .attr('x', (d,i) => paddingX + (i * ((w - paddingX) / num)))
-    .attr('y', h - paddingY)
-    .attr('width', (w - paddingX - num) / num)
-    .attr('height', 0)
-    .attr('class', 'bar')
-    .on('mouseover', function (d,i) {
-      this.setAttribute('style', 'fill: #0ce');
-      let svg_y = document.getElementById('svg-cart').getBoundingClientRect().top;
-      let xpos = parseFloat(d3.select(this).attr('x'));
-      let ypos = parseFloat(d3.select(this).attr('y')) + parseFloat(d3.select(this).attr('height'))/4 + svg_y;
-      d3.select('#tooltip')
-        .style('left', xpos + 'px')
-        .style('top', ypos + 'px')
-        .select('#tooltip_valor')
-        .html(
-          '<b>' + banco.substring(banco.indexOf('-') + 2, banco.length) + ':</b> '+
-          formatoFecha(datos['fechas_js'][i]) +'<br>' +
-          'Total cartera: ' + moneda.format(d) + '<br>' +
-          'ICC: ' + parseFloat(100*(datos['ICC'][i])).toFixed(2) + '%');
-      d3.select('#tooltip').classed('hidden', false);
-      })
-    .on('mouseout', function (d) {
-      let m = (h - escalaY(d) - paddingY)/(h - paddingY);
-      let c = 'fill: rgb('+ 20*m + ', ' + 80*m + ', ' + 180*m + ')';
-      this.setAttribute('style', c);
-      d3.select('#tooltip').classed('hidden', true);
-     })
-    ;
-
-    bars
-    .transition()
-    .duration(300)
-    .attr('y', d => escalaY(d))
-    .attr('height', d => h - escalaY(d) - paddingY)
-    .style('fill', d => {
-      let m = (h - escalaY(d) - paddingY)/(h - paddingY);
-      return 'rgb('+ 20*m + ', ' + 80*m + ', ' + 180*m + ')';
-    });
-
-    svg_cart
-      .selectAll('text.labels')
-      .data(datos['ICC'])
-      .enter()
-      .append('text')
-      .attr('text-anchor', 'middle')
-      .attr('x', (d,i) => paddingX + (i * ((w - paddingX) / num)) + ((w - paddingX - num) / num)/2)
-      .attr('y', (d,i) => escalaY(datos['cartera_total'][i]) + 30)
-      .attr('class', 'label')
-      .text(d => String((100 * parseFloat(d)).toFixed(1)));
-
   svg_cart
     .append('g')
     .attr('transform', 'translate(' + paddingX + ', 0)')
@@ -119,9 +62,17 @@ function draw(datos_global) {
     .attr('class', 'tickX')
     .selectAll('text')
     .attr('transform', 'rotate(-90) translate(-' + paddingY + ', 0)');
-  d3.select('#csv')
-    .attr('download', banco.substring(banco.indexOf('-')+2, banco.length) + '.csv')
-    .attr('href','data:text/csv;base64,' + datos['csv'])
+
+    let linea = d3.line()
+      .x((d,i) => paddingX + (i * ((w - paddingX) / num)))
+      .y(d => escalaY(d));
+
+  svg_cart
+      .append('path')
+      .attr('d', linea(datos['resultados']))
+      .style("stroke-width", 2)
+      .style("stroke", "steelblue")
+      .style("fill", "none");
 }
 
 window.onload = () => document.getElementById('lista').onchange = reset;
